cf1b905dd50e45f37b6eefc32245c2506519a8a9
aswinbaral2 <aswinbaral2@gmail.com>
2025-12-15 23:07:49 +0545

    task completed

---
+++ TypeScript/script.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var age = 21;
+var isStudent = true;
+var userName = "aswin";
+console.log(userName);
+console.log(age);
---
+++ TypeScript/script.ts
@@ -0,0 +1,8 @@
+
+export {};
+let age: number = 21;
+
+let isStudent: boolean = true;
+let userName = "aswin";
+console.log(userName);
+console.log(age);
