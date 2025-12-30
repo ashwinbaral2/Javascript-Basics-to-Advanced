f2603c1bb35f34ff5c48a8bde62d54e8256b4f8d
ashwinbaral2 <aswinbaral2@gmail.com>
2025-12-30 17:43:13 +0545

    added my project

---
+++ my-project/app/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HOME = () => {
+  return (
+    <div style={{
+      backgroundColor:'green',
+      fontSize:'30px',
+      fontFamily:'cursive',
+      padding:'auto',
+      margin:'auto',
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      height:'50vh',
+      width: '50vh',
+      textAlign: 'center'
+      
+    }}>This is home page.</div>
+  )
+}
+
+export default HOME
