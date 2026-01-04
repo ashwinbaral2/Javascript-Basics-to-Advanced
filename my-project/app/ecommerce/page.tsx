12c03b2d23b16de87e7ab31ce5276d5d928c2bb3
ashwinbaral2 <aswinbaral2@gmail.com>
2026-01-04 08:26:54 +0545

    Jan 4 2026 note entry

---
+++ my-project/app/ecommerce/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ecommerce = () => {
+    return (
+        <div>
+            <div className='container min-h-screen min-w-screen bg-gray-900'>
+                <div className="container-wrapper flex flex-col">
+                    <div className='products-wrapper '></div>
+                    <div className='products-wrapper '></div>
+                    <div className='products-wrapper '></div>
+                    <div className='products-wrapper '></div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ecommerce
